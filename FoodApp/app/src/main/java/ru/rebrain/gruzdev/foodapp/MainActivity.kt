6bed2ee902052ca4ba2adec510788934e1432ea9
ouril <ourilkreg@gmail.com>
2019-03-29 16:26:24 +0300

    added CoroutuneScope interface for Activity

--- FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/MainActivity.kt
+++ FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlin.coroutines.CoroutineContext
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/splash/SplashActivity.kt
+++ FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/splash/SplashActivity.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import kotlinx.coroutines.*
 import ru.rebrain.gruzdev.foodapp.MainActivity
 import ru.rebrain.gruzdev.foodapp.R
+import kotlin.coroutines.CoroutineContext
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity: AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext = CoroutineScope(Dispatchers.Default).coroutineContext
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val scope = CoroutineScope(Dispatchers.Default)
-        scope.launch {
+
+        launch {
             delay(500)
             MainActivity.start(this@SplashActivity)
             finish()
         }
     }
+
+
 }
