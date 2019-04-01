294fd83fb28653fdc22e23ff4129e34300283ea8
ouril <ourilkregg@gmail.com>
2019-04-01 21:38:50 +0300

    Revert "Revert "Splash""

--- FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/intro/IntroActivity.kt
+++ FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/intro/IntroActivity.kt
@@ -1,24 +1,23 @@
-package ru.rebrain.gruzdev.foodapp
+package ru.rebrain.gruzdev.foodapp.screen.intro
 
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.CoroutineScope
-import kotlin.coroutines.CoroutineContext
+import ru.rebrain.gruzdev.foodapp.MainActivity
+import ru.rebrain.gruzdev.foodapp.R
 
-
-class MainActivity : AppCompatActivity() {
-
+class IntroActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
+
     }
 
     companion object {
         fun start(context: Context) {
-            val intent = Intent(context, MainActivity::class.java)
+            val intent = Intent(context, IntroActivity::class.java)
             context.startActivity(intent)
         }
     }
--- FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/splash/SplashActivity.kt
+++ FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/splash/SplashActivity.kt
@@ -1,26 +1,43 @@
 package ru.rebrain.gruzdev.foodapp.screen.splash
 
-import android.content.Intent
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import ru.rebrain.gruzdev.foodapp.MainActivity
 import ru.rebrain.gruzdev.foodapp.R
+import ru.rebrain.gruzdev.foodapp.screen.intro.IntroActivity
+import kotlin.coroutines.CoroutineContext
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), CoroutineScope {
+    override val coroutineContext: CoroutineContext = CoroutineScope(Dispatchers.Default).coroutineContext
+    val PREFERENCE_FILE = R.string.preference_file_key.toString()
+    val IS_A_FIRST_TIME = R.string.preference_is_first_time.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val sharedPref = getSharedPreferences(
+            PREFERENCE_FILE, Context.MODE_PRIVATE
+        )
 
-        GlobalScope.launch {
+        launch {
             delay(500)
-
-            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            this@SplashActivity.finish()
+            startIntroOrMain(sharedPref)
+            finish()
         }
+    }
+
+    private fun SplashActivity.startIntroOrMain(sharedPref: SharedPreferences) {
+        if (sharedPref.getBoolean(IS_A_FIRST_TIME, true)) {
+
+            with(sharedPref.edit()) {
+                putBoolean(IS_A_FIRST_TIME, false)
+                apply()
+            }
+            IntroActivity.start(this@SplashActivity)
+        } else MainActivity.start(this@SplashActivity)
     }
 
 
