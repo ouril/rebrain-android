5252c9e226966a4ec90acaded8150c1a382afed4
ouril <ourilkreg@gmail.com>
2019-04-01 21:08:49 +0300

    added intro activity

---
+++ FoodApp/app/src/main/java/ru/rebrain/gruzdev/foodapp/screen/intro/IntroActivity.kt
@@ -0,0 +1,24 @@
+package ru.rebrain.gruzdev.foodapp.screen.intro
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.rebrain.gruzdev.foodapp.MainActivity
+import ru.rebrain.gruzdev.foodapp.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+    }
+
+    companion object {
+        fun start(context: Context) {
+            val intent = Intent(context, IntroActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
